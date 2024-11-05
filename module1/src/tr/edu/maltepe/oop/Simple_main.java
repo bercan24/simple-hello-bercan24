e603835ef795074777cf78079233d4f8f01c6cf7
bercan24 <berkanercan2003.be@gmail.com>
2024-11-05 18:42:10 +0300

    Update Simple_main.java
    230706307

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,10 +1,8 @@
 package tr.edu.maltepe.oop;
-public  class imple_main {
+public  class Simple_main {
 
  public static void main(String []args){
         //
-       System.out.println("simple main.....");
+       System.out.println("Berkan Ercan 230706307");
         }
 }
-
-
